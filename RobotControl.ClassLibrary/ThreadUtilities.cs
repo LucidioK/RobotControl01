3130d3c0c26d908a95d7d13a0e5166ecfe9ab93f
Lucidio Kuhn <lucidiok@hotmail.com>
2021-01-26 09:17:14 -0800

    Removing deadlock from SerialPortImpl.

---
+++ RobotControl.ClassLibrary/ThreadUtilities.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace RobotControl.ClassLibrary
+{
+    public static class ThreadUtilities
+    {
+        public static bool ThreadWithTimeout(ThreadStart action, int timeoutInMilliseconds)
+        {
+            var thread = new Thread(action);
+            thread.Start();
+            return thread.Join(timeoutInMilliseconds);
+        }
+    }
+}

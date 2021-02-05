3e28b072e1d2862845e3562e1eca9c2c7b89e6ce
Lucidio Kuhn <lucidiok@hotmail.com>
2021-02-05 12:58:14 -0800

    Updating ReadSerial to use the class library

--- ReadSerial/Program.cs
+++ ReadSerial/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using RobotControl.ClassLibrary;
+
+using System;
 using System.IO.Ports;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace ReadSerial
 {
@@ -8,20 +11,25 @@ namespace ReadSerial
     {
         static void Main(string[] args)
         {
-            var re = new Regex("^(COM[0-9]+|com[0-9]+)$");
-            if (args == null || args.Length == 0 || !re.IsMatch(args[0]))
+            var re = new Regex("^([0-9]+)$");
+            if (args == null || args.Length != 2 || !re.IsMatch(args[0]) || !re.IsMatch(args[1]))
             {
-                Console.WriteLine("ReadSerial SerialPort\nExample:\nReadSerial COM3\n");
+                Console.WriteLine("ReadSerial SerialPort BaudRate\nExample:\nReadSerial 3 115200\n");
                 Environment.Exit(1);
             }
-            var sp = new SerialPort(args[0]);
-            sp.ReadTimeout = 1000;
-            sp.Open();
+            ISerialPort sp = new SerialPortImpl();
+            sp.Open(int.Parse(args[0]), int.Parse(args[1]), onDataReceived, onException);
+
             while (true)
             {
-                string s = sp.ReadLine();
-                Console.WriteLine(s);
+                Thread.Sleep(10);
             }
         }
+
+        private static void onException(Exception ex) =>
+            Console.WriteLine($"EX> {ex.Message}");
+
+        private static void onDataReceived(string data) =>
+            Console.WriteLine($"DT> {data}");
     }
 }
